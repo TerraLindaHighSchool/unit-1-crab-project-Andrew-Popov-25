d5f4e9a2aaa19c7935ab48d2670622597adf4018
Andrew-Popov-25 <89489268+Andrew-Popov-25@users.noreply.github.com>
2021-08-26 15:20:14 -0700

    Completed Crab Activity 2 - Moving Around

--- Crab.java
+++ Crab.java
@@ -2,12 +2,41 @@ import greenfoot.*;
 
 /**
  * This class defines a crab. Crabs live on the beach.
+ * @author Andrew Popov
+ * @version 08/26/2021
  */
 public class Crab extends Actor
 {
+    //This method repeats the following actions
     public void act()
     {
-        // Add your action code here
+        move(3);
+        turnAtEdge();
+        checkKeyPress();
+        onCollision();
+    }
+    
+    //Moves The Crab
+    
+    //Turns the Crab at the edge
+    public void turnAtEdge()
+    {
+        if(isAtEdge())
+        {
+            turn(50);
+        }
+    }
+    
+    //Ckeck for user key presses so user can turn the Crab
+    public void checkKeyPress()
+    {
+        
+    }
+    
+    //Check for collisions with other objects
+    public void onCollision()
+    {
+        
     }
 }
 
