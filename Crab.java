f859ecbec191ddafd6eb69572378e9ebf4702f1c
Andrew-Popov-25 <89489268+Andrew-Popov-25@users.noreply.github.com>
2021-08-31 14:46:08 -0700

    Completed Crab Project Challenge
    I hope that's how you spell it

--- Crab.java
+++ Crab.java
@@ -10,7 +10,6 @@ public class Crab extends Actor
     //This method repeats the following actions
     public void act()
     {
-        move(3);
         turnAtEdge();
         checkKeyPress();
         onCollision();
@@ -23,24 +22,28 @@ public class Crab extends Actor
     {
         if(isAtEdge())
         {
-            turn(50);
+            //bruh
         }
     }
     
     //Ckeck for user key presses so user can turn the Crab
     public void checkKeyPress()
     {
-        if(Greenfoot.isKeyDown("right"))
+         if (Greenfoot.isKeyDown("right"))
         {
-            turn(4);
+            setLocation(getX()+5,getY());
         }
-        if(Greenfoot.isKeyDown("left"))
+        if (Greenfoot.isKeyDown("left"))
         {
-            turn(-4);
+            setLocation(getX()-5,getY());
         }
         if(Greenfoot.isKeyDown("up"))
         {
-            Greenfoot.delay(60);
+            setLocation(getX(),getY()-5);
+        }
+        if(Greenfoot.isKeyDown("down"))
+        {
+            setLocation(getX(), getY()+5);
         }
     }
     
